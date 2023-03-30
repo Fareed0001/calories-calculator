220d7a9b85dbe61173845daa02ff10b89a47da0c
Farid Ibrahim <fareedibrahim0003@gmail.com>
2023-03-30 14:02:50 +0100

    Dashboard media query

--- index.js
+++ index.js
@@ -41,12 +41,10 @@ $("#todays-date").html(day);
 
 
 //LEARN MORE 
-$(".learn-more-div, .back-to-dashboard").slideUp();
+$(".learn-more-div, .back-to-dashboard, .back-to-details").slideUp();
 $(".first-container-text").click(function () {
-    $(".first-container-row").slideUp("slow");
-    $(".learn-more-div").slideDown("slow");
-    $(".second-container-div").slideUp("slow");
-    $(".back-to-dashboard").slideDown("slow");
+    $(".first-container-row, .second-container-div").slideUp("slow");
+    $(".learn-more-div, .back-to-dashboard").slideDown("slow");
 });
 // LEARN MORE END
 
@@ -54,9 +52,8 @@ $(".first-container-text").click(function () {
 // FRUITS AND VEGETABLES 
 $(".fruit-and-veg-div, .fruit-and-veg-text1, .fruit-and-veg-text2").slideUp();
 $(".more-food").click(function () {
-    $(".fruit-and-veg-div").slideDown("slow");
+    $(".fruit-and-veg-div, .back-to-dashboard").slideDown("slow");
     $(".first-container-row, .second-container-div").slideUp("slow");
-    $(".back-to-dashboard").slideDown("slow");
 
     $(".fruit-and-veg-col1").hover(function () {
         $(".fruit-and-veg-img1").slideUp("slow");
@@ -77,21 +74,28 @@ $(".more-food").click(function () {
 
 // RAW FRUITS DETAILS
 $(".raw-fruits").slideUp();
-$(".fruit-and-veg-col1").click(function() {
+$(".fruit-and-veg-col1").click(function () {
     $(".fruit-and-veg-div").slideUp("slow");
-    $(".raw-fruits").slideDown("slow");
+    $(".raw-fruits, .back-to-details").slideDown("slow");
 });
 // RAW FRUITS DETAILS
 
 
 // RAW VEGETABLES DETAILS
 $(".raw-vegetables").slideUp();
-$(".fruit-and-veg-col2").click(function() {
+$(".fruit-and-veg-col2").click(function () {
     $(".fruit-and-veg-div").slideUp("slow");
-    $(".raw-vegetables").slideDown("slow");
+    $(".raw-vegetables, .back-to-details").slideDown("slow");
 });
 // RAW VEGETABLES DETAILS
 
+
+//BACK TO DETAILS BUTTON
+$(".back-to-details").click(function () {
+    $(".raw-vegetables, .raw-fruits, .back-to-details").slideUp("slow");
+    $(".fruit-and-veg-div").slideDown("slow");
+});
+//BACK TO DETAILS BUTTON
 
 
 //BACK TO DASHBOARD BUTTON
@@ -101,3 +105,28 @@ $(".back-to-dashboard").click(function () {
 });
 //BACK TO DASHBOARD BUTTON
 
+if (window.matchMedia("(max-width: 992px)").matches) {
+    //LEARN MORE 
+    $(".first-container-text, .more-food").click(function () {
+        $(".second-col-header, .today-container, .todays-meals").slideUp("slow");
+    });
+    // LEARN MORE END
+
+    // FRUITS AND VEGETABLES 
+    // $(".more-food").click(function () {
+        
+    // });
+    // FRUITS AND VEGETABLES END 
+
+
+
+
+
+
+    //BACK TO DASHBOARD BUTTON
+    $(".back-to-dashboard").click(function () {
+        $(".second-col-header, .today-container, .todays-meals").slideDown("slow");
+        $(".back-to-details").slideUp("slow");
+    });
+    //BACK TO DASHBOARD BUTTON
+}
